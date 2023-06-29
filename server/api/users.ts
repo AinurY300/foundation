8966a3348fc7a792486e866297133326077d759b
Ainur Yarulin <yarulinainur@gmail.com>
2023-06-29 15:37:15 +0700

    test

--- server/api/users.ts
+++ server/api/users.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from '@prisma/client'
 
 export default defineEventHandler(async event => {
 
-  const prisma = new PrismaClient()
-  const users = await prisma.user.findMany()
+  // const prisma = new PrismaClient()
+  // const users = await prisma.user.findMany()
 
   return {
-    users: users
+    users: 'em users'
   }
 })
