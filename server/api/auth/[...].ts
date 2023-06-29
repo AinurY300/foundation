7c310f6ecc24e66872cbdf30b3eddcd558fb1a21
Ainur Yarulin <yarulinainur@gmail.com>
2023-06-30 00:51:44 +0700

    ✅ ADD NEXTAUTH WITH PRISMA ADAPTER

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,8 +1,13 @@
 import { NuxtAuthHandler } from '#auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from '@/prisma/db'
+
 import YandexProvider from 'next-auth/providers/yandex'
 
 export default NuxtAuthHandler({
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     YandexProvider.default({
